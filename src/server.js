1bd39b59346ede438f2f1bf07d908e4e9c495bc9
SachKar <88773679+sachham-karki@users.noreply.github.com>
2022-09-05 01:29:00 +1000

    Releasing site

---
+++ src/server.js
@@ -0,0 +1,101 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import { MongoClient } from "mongodb";
+import path from "path";
+import { fileURLToPath } from "url";
+//const mongoose = require("mongoose");
+//const { config } = require("dotenv");
+const app = express();
+
+const PORT = 8000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
+
+const withDB = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+    });
+    const db = client.db("my-blog");
+
+    await operations(db);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+};
+
+//article end point
+app.get("/api/articles/:name", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  }, res);
+});
+
+//up voting end point
+app.post("/api/articles/:name/upvote", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          upvotes: articleInfo.upvotes + 1,
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
+});
+
+//add comments end point
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  withDB(async (db) => {
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Port is listen at ${PORT}!`);
+});
